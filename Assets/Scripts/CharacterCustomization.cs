824bdd9b83377ade135586cd28add2b6b12d6b3c
hpruthi1 <46549782+hpruthi1@users.noreply.github.com>
2020-04-24 09:20:02 +0530

    Implementing Character Customization

--- Assets/Scripts/CharacterCustomization.cs
+++ Assets/Scripts/CharacterCustomization.cs
@@ -5,6 +5,10 @@ using Mirror;
 
 public class CharacterCustomization : MonoBehaviour
 {
+    #region Private .
+    private PlayerInstance _playerInstance;
+    #endregion
+
     public GameObject Alex;
     public GameObject Bryce;
     public GameObject James;
@@ -12,35 +16,105 @@ public class CharacterCustomization : MonoBehaviour
     public GameObject Player2;
     public GameObject Player3;
 
+
+    private void Awake()
+    {
+        
+            
+    }
+
+    public void SelectCharacter(CharacterSelection selection)
+    {
+        _playerInstance.PlayerSpawner.TrySpawn(selection.CharacterType);
+    }
+
     private void Start()
     {
-        James.SetActive(true);
-        Bryce.SetActive(false);
-        Alex.SetActive(false);
+        //if (isLocalPlayer)
+        {
+            James.SetActive(true);
+            Bryce.SetActive(false);
+            Alex.SetActive(false);
+        }
     }
 
     public void onCharacter1Button()
     {
-        FindObjectOfType<NetworkManager>().playerPrefab = Player1;
-        James.SetActive(true);
-        Bryce.SetActive(false);
-        Alex.SetActive(false);
+        //if (isLocalPlayer)
+        {
+            FindObjectOfType<NetworkManager>().playerPrefab = Player1;
+            //CmdPlayer1Spawn();
+            James.SetActive(true);
+            Bryce.SetActive(false);
+            Alex.SetActive(false);
+        }
     }
 
     public void onCharacter2Button()
     {
-        FindObjectOfType<NetworkManager>().playerPrefab = Player2 ;
-        James.SetActive(false);
-        Bryce.SetActive(true);
-        Alex.SetActive(false);
+        //if (isLocalPlayer)
+        {
+            FindObjectOfType<NetworkManager>().playerPrefab = Player2;
+            //CmdPlayer2Spawn();
+            James.SetActive(false);
+            Bryce.SetActive(true);
+            Alex.SetActive(false);
+        }
     }
 
     public void onCharacter3Button()
     {
-        James.SetActive(false);
-        Bryce.SetActive(false);
-        Alex.SetActive(true);
-        FindObjectOfType<NetworkManager>().playerPrefab = Player3;
+       // if (isLocalPlayer)
+        {
+            James.SetActive(false);
+            Bryce.SetActive(false);
+            Alex.SetActive(true);
+            FindObjectOfType<NetworkManager>().playerPrefab = Player3;
+            //CmdPlayer3Spawn();
+        }
     }
 
+    /*
+    [Command]
+
+    void CmdPlayer1Spawn()
+    {
+        RpcPlayer1Spawn();
+    }
+
+    [ClientRpc]
+
+    void RpcPlayer1Spawn()
+    {
+        FindObjectOfType<NetworkManager>().playerPrefab = Player1;
+    }
+
+    [Command]
+
+    void CmdPlayer2Spawn()
+    {
+        RpcPlayer2Spawn();
+    }
+
+    [ClientRpc]
+
+    void RpcPlayer2Spawn()
+    {
+        FindObjectOfType<NetworkManager>().playerPrefab = Player2;
+    }
+
+    [Command]
+
+    void CmdPlayer3Spawn()
+    {
+        RpcPlayer3Spawn();
+    }
+
+    [ClientRpc]
+
+    void RpcPlayer3Spawn()
+    {
+        FindObjectOfType<NetworkManager>().playerPrefab = Player3;
+    }
+    */
 }
